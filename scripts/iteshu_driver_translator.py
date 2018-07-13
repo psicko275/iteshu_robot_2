0dc67c6efaeec1a3ae87f8395ee76b0f1d6bee55
Jesús Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2018-07-12 20:00:32 -0500

    Adding function to compute wheel speeds from robot's speed using the differential drive model

--- scripts/iteshu_driver_translator.py
+++ scripts/iteshu_driver_translator.py
@@ -12,6 +12,12 @@ from std_msgs.msg import Int16
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
+class IteshuRobot():
+    def __init__(self):
+        self.R=0.1   #Wheel radius [m]
+        self.L=0.4   #Distance between the wheels [m]
+        
+
 class IteshuDriver():
     def __init__(self):
         rospy.on_shutdown(self.cleanup)
@@ -22,24 +28,24 @@ class IteshuDriver():
         self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=1)
         print "Publishers ok"
         print "Starting Node..."
-        self.x = 0
-        self.SPEED = 1.5 	#[rad/s]
-        self._speed = 0		#temp speed
-        self.odometry = Odometry()
-
+        
         ############################### SUBSCRIBERS #####################################
         rospy.Subscriber("speedMotors_float", Float32MultiArray, self.speed_f)
         rospy.Subscriber("robotPosition", Float32MultiArray, self.robotPosition_cb)
         rospy.Subscriber("uControl", Float32MultiArray, self.uControl_cb)
         rospy.Subscriber("cmd_vel", Twist, self.cmd_vel_cb)
 
-        ###********** INIT NODE **********###
-        r = rospy.Rate(10)              #10Hz
-        print "Node initialized 10hz"
-
+        ############ CONSTANTS ################
+        self.x = 0
+        self.SPEED = 1.5 	#[rad/s]
+        self._speed = 0		#temp speed
+        self.wl=0 #right wheel speed [rad/sec]
+        self.wr=0 #right wheel speed [rad/sec]
+        self.odometry = Odometry()
+        self.robot=IteshuRobot()
+        self.setpoint = Float32MultiArray() ##Array that contains the wheel speeds [lef][right]
+       
         ###******** SETTING ARRAYS *******###
-
-        self.setpoint = Float32MultiArray()
         self.setpoint.layout.dim.append(MultiArrayDimension())
         self.setpoint.layout.dim[0].label = "height"
         self.setpoint.layout.dim[0].size = 2
@@ -48,13 +54,14 @@ class IteshuDriver():
         self.setpoint.data = [0]*2
 
         ###********** INIT ARRAYS ********###
-
-        self.setpoint.data[0]= 5.4
-        self.setpoint.data[1]= 6.8
-
+        self.setpoint.data[0]= 0
+        self.setpoint.data[1]= 0
+        
+        ###********** INIT NODE **********###
+        r = rospy.Rate(10)              #10Hz
+        print "Node initialized 10hz"
         print(self.setpoint.data[0])
         print(self.setpoint.data[1])
-
         while not rospy.is_shutdown():
             self.pub_odom.publish(self.odometry)
             r.sleep()
@@ -73,8 +80,20 @@ class IteshuDriver():
 	    #file3.write(str(data.data[0])+"\t"+str(data.data[1])+"\t"+str(data.data[2])+"\n")
 
     def cmd_vel_cb(self, data):
-        ##TODO: calcular la velocidad de las ruedas apartir de la velocidad del robot
-        print(data)
+        ## This function computes the wheel speed from the robot's speed
+        ##      2v-Lw        2v+Lw
+        ## wl = -----;  wr = ------
+        ##        2R           2R
+        v= data.linear.x
+        w= data.angular.z
+        self.wl=(2*v-self.robot.L*w)/(2*self.robot.R) #right wheel speed (rad/sec)
+        self.wr=(2*v+self.robot.L*w)/(2*self.robot.R) #right wheel speed (rad/sec)
+        print "wl= ", self.wl
+        print "wl= ", self.wr
+        self.setpoint.data[0]= self.wl
+        self.setpoint.data[1]= self.wr
+        self.pub_f.publish(self.setpoint)
+        
     
     def cleanup(self):
         #Stop the robot before aborting the node	
